b12e49bba9f1adb3a5bfbee8acbc6247867fc347
humbleman <1753930471@qq.com>
2024-05-14 00:46:42 +0800

    upload bfs and dijkstra

--- basic algorithm/debug.py
+++ basic algorithm/debug.py
@@ -1,24 +1,44 @@
-n, m = map(int,input().split())
-maze = []
-for i in range(n):
-    row = list(map(int,input().split()))
-    maze.append(row)
+# use heap to maintain the smallest point to expand in the next iteration.
+from queue import PriorityQueue
 
-def bfs():
-    while len(fringe) > 0:
-        point = fringe.pop(0)
-        if point[:2] == (n-1,m-1):
-            print(point[-1])
-        for d in direction:
-            new = (point[0]+d[0],point[1]+d[1])
-            if 0<=new[0]<n and 0<=new[1]<m and new not in visited:
-                if not maze[new[0]][new[1]]:
-                    visited.add(new)
-                    fringe.append(new+(point[-1]+1,))
+N = 150010
+h, e, ne, w = [-1] * N, [-1] * N, [-1] * N, [0] * N
+idx = 0
+q = PriorityQueue()
+visit = set()
+dist = [0x3f3f3f,0] + [0x3f3f3f] * N
 
-direction = [(-1,0),(0,1),(1,0),(0,-1)]
-fringe = [(0,0,0)]                               # need and another value to determine the path!!!
-visited = set()
-visited.add((0,0))
-path = 0
-bfs()
+def add(a,b,z):
+    global idx
+    e[idx] = b
+    ne[idx] = h[a]
+    w[idx] = z
+    h[a] = idx
+    idx += 1
+
+def dijkstra():
+    q.put((dist[1],1))
+    while not q.empty():
+        d, var = q.get()
+        if var in visit: continue          # important
+        visit.add(var)
+        next = h[var]
+        while next != -1:
+            j = e[next]
+            if dist[j] > d + w[next]:
+                dist[j] = d + w[next]
+                q.put((dist[j],j))
+            next = ne[next]
+    if dist[n] == 0x3f3f3f:
+        return -1
+    return dist[n]
+
+
+
+
+n,m = map(int,input().split())
+for i in range(m):
+    a,b,z = map(int,input().split())
+    add(a,b,z)
+
+print(dijkstra())
