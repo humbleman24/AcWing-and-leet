a5a294ef57a5da7f7430f540cb7feb1c81833833
humbleman <1753930471@qq.com>
2024-05-15 00:23:28 +0800

    upload bellman-ford and spfa

--- basic algorithm/debug.py
+++ basic algorithm/debug.py
@@ -1,12 +1,10 @@
-# use heap to maintain the smallest point to expand in the next iteration.
-from queue import PriorityQueue
-
-N = 150010
-h, e, ne, w = [-1] * N, [-1] * N, [-1] * N, [0] * N
+from collections import deque
+N,M = 100010, 100010
+h,e,ne,w = [-1]*N, [-1]*N,[-1]*N,[0] * N
 idx = 0
-q = PriorityQueue()
-visit = set()
-dist = [0x3f3f3f,0] + [0x3f3f3f] * N
+dist = [0x3f3f3f] * N
+count = [0] * N
+dist[1] = 0
 
 def add(a,b,z):
     global idx
@@ -16,29 +14,33 @@ def add(a,b,z):
     h[a] = idx
     idx += 1
 
-def dijkstra():
-    q.put((dist[1],1))
-    while not q.empty():
-        d, var = q.get()
-        if var in visit: continue          # important
-        visit.add(var)
-        next = h[var]
-        while next != -1:
-            j = e[next]
-            if dist[j] > d + w[next]:
-                dist[j] = d + w[next]
-                q.put((dist[j],j))
-            next = ne[next]
-    if dist[n] == 0x3f3f3f:
-        return -1
-    return dist[n]
-
-
-
+def spfa():
+    visit = set()
+    q = deque()
+    for i in range(1,n+1):
+        q.append(i)
+        visit.add(i)
+    for i in range(n):
+        while len(q):
+            point = q.popleft()
+            visit.remove(point)
+            next = h[point]
+            while next != -1:
+                j = e[next]
+                if dist[j] > dist[point] + w[next]:
+                    dist[j] = dist[point] + w[next]
+                    count[j] = count[point] + 1
+                    if count[j] >= n:
+                        return 'Yes'
+                    if j not in visit:
+                        q.append(j)
+                        visit.add(j)
+                next = ne[next]
+    return 'No'
 
 n,m = map(int,input().split())
 for i in range(m):
     a,b,z = map(int,input().split())
     add(a,b,z)
 
-print(dijkstra())
+print(spfa())
